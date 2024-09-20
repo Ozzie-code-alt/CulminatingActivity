c7f8a78bd9ab7db1a4a28dc93b42e14095e7acc0
chaolen <siaotongcharlyn@gmail.com>
2024-09-20 15:31:42 +0800

    new

--- components/Section2.tsx
+++ components/Section2.tsx
@@ -149,10 +149,58 @@ export default function index(): JSX.Element {
         </section>
 
         <footer className='flex justify-between h-[200px] px-[150px] bg-[#ff0000] items-center py-[40px]'>
-          <div className='flex-col justify-between items-center h-full text-white'>
+          <div className='flex-col justify-between items-center text-white'>
             <p>HOME</p>
             <p>ABOUT US</p>
             <p>CONTACT US</p>
+          </div>
+          <div className='py-2 text-white'>
+            <div className='flex gap-x-2'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                // stroke-width='1.5'
+                stroke='currentColor'
+                className='size-6'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  d='M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z'
+                />
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  d='M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z'
+                />
+              </svg>
+
+              <p>Iloilo Supermart - Mandurriao</p>
+            </div>
+            <div className='flex gap-x-2'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                // stroke-width='1.5'
+                stroke='currentColor'
+                className='size-6'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  d='M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z'
+                />
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  d='M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z'
+                />
+              </svg>
+
+              <p>Iloilo Supermart - Jaro</p>
+            </div>
           </div>
           <img src='/images/logo-white.png' alt='' className='h-[40px]' />
         </footer>
