7d08b402dbb5d40286073f08ee2f57d0c9faf96b
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-09-20 14:57:05 +0800

    some fixes

--- components/Section1.tsx
+++ components/Section1.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useScroll, useTransform, motion } from 'framer-motion';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import IloiloSpline from './SplineComponent';
 import SubVideoSection from './SubSectionVideo';
 
@@ -73,7 +73,7 @@ const Section2 = ({ scrollYProgress }) => {
       <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:54px_54px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'></div>
       <article className='container mx-auto relative z-10 '>
         <h1 className='text-6xl leading-[100%] py-10 font-semibold  tracking-tight '>
-          Images That doesn't Make any sense <br /> but still in this section
+          Images That doesn&apos;t Make any sense <br /> but still in this section
         </h1>
         <div className='grid grid-cols-4 gap-4'>
           <img
